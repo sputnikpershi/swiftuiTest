bbe66b2409e0c960e2cbb2a170d6ca45ef803cb8
Kiryl <66584728+sputnikpershi@users.noreply.github.com>
2023-01-21 16:59:53 +0800

    Added screens and functions

---
+++ SwiftUIProject/SwiftUIProject/SwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectApp.swift
+//  SwiftUIProject
+//
+//  Created by Kiryl Rakk on 21/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
